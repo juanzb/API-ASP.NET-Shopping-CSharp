d47f77f52ce156ca691d86c55883c7f7753398a4
juanzb <juanzarache2000@gmail.com>
2024-11-28 05:11:05 -0500

    Implementando el uso de Middlewares

--- src/Api ASP.NET Shoopping/Program.cs
+++ src/Api ASP.NET Shoopping/Program.cs
@@ -1,4 +1,5 @@
 using Services;
+using System.Text;
 using UnitOfWork.Interfaces;
 using UnitOfWork.MysqlServer;
 
@@ -19,9 +20,48 @@ builder.Services.AddScoped<ClientsServices>();
 builder.Services.AddScoped<ProductsServices>();
 
 
+
 var app = builder.Build();
 
+
+// pathBase
+app.UsePathBase("/api");
+
+
 // Configure the HTTP request pipeline.
+
+
+//Middlewere Test
+app.Use(async (context, next) =>
+{
+    Console.WriteLine(context.Request.Host);
+    Console.WriteLine(context.Request.Method);
+    Console.WriteLine(context.Request.Path);
+    Console.WriteLine(context.Request.PathBase);
+    Console.WriteLine(context.Request.Protocol);
+    Console.WriteLine(context.Request.Query);
+    Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(context.Request.Cookies));
+    Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(context.Request.Headers));
+    Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(context.Request.ContentType));
+    Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(context.Request.ContentLength));
+
+    // Habilitar relectura del cuerpo
+    context.Request.EnableBuffering();
+    // Leer el cuerpo como texto
+    using (var reader = new StreamReader(context.Request.Body, Encoding.UTF8, leaveOpen: true))
+    {
+        var body = await reader.ReadToEndAsync();
+        Console.WriteLine("Body: " + body);
+
+        // Reiniciar el flujo del cuerpo para que otros middlewares puedan leerlo
+        context.Request.Body.Position = 0;
+    }
+
+    if (context.Request.PathBase != "/api") 
+        context.Response.StatusCode = 400;
+    else 
+        await next();
+});
 
 app.UseAuthorization();
 
